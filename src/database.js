cab08d52d3bdd78da610f323349481990efacf9e
Tonathiu <tonathiu123456@gmail.com>
2022-02-24 12:37:51 -0600

    Primera version del proyecto

---
+++ src/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+//const { mongodb } = require('./keys');
+
+//mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb://127.0.0.1:27017/login', {
+  useNewUrlParser: true
+})
+  .then(db => console.log('DB is connected'))
+  .catch(err => console.log(err));
---
+++ src/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongodb: {
+    URI: 'mongodb://localhost:27017/login'
+    //mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.1.9
+  }
+};
