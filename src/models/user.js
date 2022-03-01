9245bdf09b8dc0966e4d9ec396ff191e5f3236b9
Tonathiu <tonathiu123456@gmail.com>
2022-02-28 20:50:32 -0600

    Tercer version

--- src/models/user.js
+++ src/models/user.js
@@ -4,8 +4,10 @@ const bcrypt = require('bcrypt-nodejs');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  email: String,
-  password: String
+  email: {type:String,required:true},
+  password:  {type:String,required:true},
+  admin:  {type:Boolean,required:true},
+  username:  {type:String,required:true},
 });
 
 userSchema.methods.encryptPassword = (password) => {
--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
+const { body,validationResult } = require('express-validator');
+const alert = false;
+
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -18,14 +21,22 @@ passport.use('local-signup', new LocalStrategy({
   const user = await User.findOne({'email': email})
   console.log(user)
   if(user) {
-    return done(null, false, req.flash('signupMessage', 'The Email is already Taken.'));
+    alert = true;
+    return done(null, false, req.flash('signupMessage', 'The Email is already Taken.'),alert);
   } else {
-    const newUser = new User();
-    newUser.email = email;
-    newUser.password = newUser.encryptPassword(password);
-    console.log(newUser)
-    await newUser.save();
-    done(null, newUser);
+    const pass = req.body.pass;
+    if(password==pass){
+      const newUser = new User();
+      newUser.email = email;
+      newUser.admin = 'false';
+      newUser.username = req.body.username;
+      newUser.password = newUser.encryptPassword(password);
+      console.log(newUser)
+      await newUser.save();
+      done(null, newUser);
+    }else{
+      return done(null, false, req.flash('signupMessage', 'The password is incorrect.'));
+    } 
   }
 }));
 
@@ -41,6 +52,7 @@ passport.use('local-signin', new LocalStrategy({
   if(!user.comparePassword(password)) {
     return done(null, false, req.flash('signinMessage', 'Incorrect Password'));
   }
+  user.password= user.encryptPassword(password);
   return done(null, user);
 }));
 //return done(null, false, req.flash('signinMessage', 'No User Found'));
